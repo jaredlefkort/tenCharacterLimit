5cd4e58be2512355a99a50e926f5b799d58c4d78
Jared <Jared@Jareds-MacBook-Pro.local>
2016-03-03 13:09:47 -0500

    this is the working version of the program that also contains comments

--- tenCharacterLimit.py
+++ tenCharacterLimit.py
@@ -2,21 +2,29 @@
 #Date: 3/3/2016
 
 f = open("input.txt", 'r')
-
-
+#this will open the file "input" in read mode
 
 contents = f.readlines()
+#This will read the contents of the file into a list 
 
 f.close()
+#this will close the file
 
 f = open("output.txt", 'r+')
+#this will open the file "output" in read and write mode
 
 for item in contents:
+	
 	item = item[0:10]
+	#this will only print/add/read the first ten characters of each item in contents
+	
 	item = item.strip("\n")
 	
 	f.write(item)
+	#this will write each item to the file
+	
 	print(item)
-
+	#this will print the first ten characters of each item
+	
 f.close()
-	 
+#this will close the file
